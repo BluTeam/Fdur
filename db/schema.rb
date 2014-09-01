9bf1639a2e4a680143b0e9d7921e3934e363a3d0
Yu Zhi Tong <yuzt@cimu.com.cn>
2014-09-01 17:19:45 +0800

    devise登录邮件确认

--- db/schema.rb
+++ db/schema.rb
@@ -24,10 +24,15 @@ ActiveRecord::Schema.define(version: 20140901055752) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
