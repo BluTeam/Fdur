2b3d88ab8d32301a427d04f3c0d59ffa8d9a3139
Yu Zhi Tong <yuzt@cimu.com.cn>
2014-09-01 14:34:20 +0800

    devise_sample_demo

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: "home#index"
 
+  #resources :users
+  #devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
