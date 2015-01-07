2c1249cef8945af3090b0f3bd0c51f4372afd718
Scott1743 <512981271@qq.com>
2015-01-07 19:56:56 +0800

    完成主页、服务条款页面

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root to: "home#index"
+  get :term, to: "home#term"
 
   #resources :users
   #devise_for :users, controllers: { sessions: 'users/sessions' }
